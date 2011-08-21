787c0e5f118036c25eef98f58ef05fccc1483f87
Douglas Burke <dburke@cfa.harvard.edu>
2011-08-21 12:11:47 -0400

    Move snEq/Le into instances. Minor doc updates
    --HG--
    branch : intern-branch

--- Swish/Utils/Namespace.hs
+++ Swish/Utils/Namespace.hs
@@ -115,7 +115,7 @@ namespaceToBuilder (Namespace pre uri) =
 --  ScopedName, made from a namespace and a local name
 ------------------------------------------------------------
 
--- |A full ScopedName value has a QName prefix, namespace URI
+-- | A full ScopedName value has a QName prefix, namespace URI
 --  and a local part.  ScopedName values may omit the prefix
 --  (see 'Namespace') or the local part.
 --
@@ -124,15 +124,19 @@ namespaceToBuilder (Namespace pre uri) =
 --
 data ScopedName = ScopedName !QName Namespace T.Text
 
+-- | Returns the local part.
 getScopeLocal :: ScopedName -> T.Text
 getScopeLocal (ScopedName _ _ l) = l
 
+-- | Returns the namespace.
 getScopeNamespace :: ScopedName -> Namespace
 getScopeNamespace (ScopedName _ ns _) = ns
 
+-- | Returns the prefix of the namespace, if set.
 getScopePrefix :: ScopedName -> Maybe T.Text
 getScopePrefix = getNamespacePrefix . getScopeNamespace
 
+-- | Returns the URI of the namespace.
 getScopeURI :: ScopedName -> URI
 getScopeURI = getNamespaceURI . getScopeNamespace
 
@@ -142,34 +146,30 @@ instance IsString ScopedName where
     maybe (error ("Unable to convert " ++ s ++ " into a ScopedName"))
           makeURIScopedName (parseURIReference s)
     
+-- | Scoped names are equal if their corresponding QNames are equal
 instance Eq ScopedName where
-    (==) = snEq
+  (ScopedName qn1 _ _) == (ScopedName qn2 _ _) = qn1 == qn2
 
+-- | Scoped names are ordered by their QNames
 instance Ord ScopedName where
-    (<=) = snLe
+  (ScopedName qn1 _ _) <= (ScopedName qn2 _ _) = qn1 <= qn2
 
+-- | If there is a namespace associated then the Show instance
+-- uses @prefix:local@, otherwise @<url>@.
 instance Show ScopedName where
-    show (ScopedName _ n l) = case getNamespacePrefix n of
+    show (ScopedName qn n l) = case getNamespacePrefix n of
       Just pre -> T.unpack $ mconcat [pre, ":", l]
-      _        -> "<" ++ show (getNamespaceURI n) ++ T.unpack l ++ ">"
+      _        -> show qn -- "<" ++ show (getNamespaceURI n) ++ T.unpack l ++ ">"
 
---  Scoped names are equal if their corresponding QNames are equal
-snEq :: ScopedName -> ScopedName -> Bool
-snEq s1 s2 = getQName s1 == getQName s2
-
---  Scoped names are ordered by their QNames
-snLe :: ScopedName -> ScopedName -> Bool
-snLe s1 s2 = getQName s1 <= getQName s2
-
--- |Get QName corresponding to a scoped name
+-- |Get the QName corresponding to a scoped name.
 getQName :: ScopedName -> QName
 getQName (ScopedName qn _ _) = qn
 
--- |Get URI corresponding to a scoped name (using RDF conventions)
+-- |Get URI corresponding to a scoped name (using RDF conventions).
 getScopedNameURI :: ScopedName -> URI
 getScopedNameURI = getQNameURI . getQName
 
--- for the moment leave this as String rather than Text
+-- for the moment leave matchName using String rather than Text
 
 -- |Test if supplied string matches the display form of a
 --  scoped name.
@@ -181,14 +181,7 @@ makeScopedName :: Maybe T.Text -> URI -> T.Text -> ScopedName
 makeScopedName pre nsuri local =
   ScopedName (newQName nsuri local) (Namespace pre nsuri) local
 
-{-
-TODO: should just pass URIs around.
-
-At the moment support the use of URI references.  Unclear of semantics
-to know whether this is sensible (probably is, but should look at).
--}
-
--- |Construct a ScopedName from a QName
+-- |Construct a ScopedName from a QName.
 makeQNameScopedName :: Maybe T.Text -> QName -> ScopedName
 makeQNameScopedName pre qn = ScopedName qn (Namespace pre (getNamespace qn)) (getLocalName qn)
 
